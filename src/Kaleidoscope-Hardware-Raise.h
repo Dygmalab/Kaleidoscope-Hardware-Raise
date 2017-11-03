2fce343873a23f1f2e234929f1adff0bc483f9f9
matt venn <matt@mattvenn.net>
2017-11-03 11:19:56 +0100

    removed key from 4th row on left side

--- src/Kaleidoscope-Hardware-Raise.h
+++ src/Kaleidoscope-Hardware-Raise.h
@@ -240,14 +240,14 @@ class Raise {
   r0c7, r0c6, r0c5, r0c4, r0c3, r0c2, r0c1,         r0c9,  r0c10, r0c11, r0c12, r0c13, r0c14, r0c15, \
   r1c7, r1c6, r1c5, r1c4, r1c3, r1c2,               r1c9,  r1c10, r1c11, r1c12, r1c13, r1c14, r1c15, r1c8, \
   r2c7, r2c6, r2c5, r2c4, r2c3, r2c2,               r2c9,  r2c10, r2c11, r2c12, r2c13, r2c14, r2c15, \
-  r3c7, r3c6, r3c5, r3c4, r3c3, r3c2, r3c1,                r3c10, r3c11, r3c12, r3c13, r3c14, r3c15, \
+  r3c7, r3c6, r3c5, r3c4, r3c3, r3c2,                      r3c10, r3c11, r3c12, r3c13, r3c14, r3c15, \
   r4c7, r4c6, r4c5, r4c4,             r4c3,         r4c11,               r4c12, r4c13, r4c14, r4c15, r4c8, \
                           r4c2, r4c1,                      r4c10, r4c9)                      \
   {                                                                                                 \
     {XXX, r0c1, r0c2, r0c3, r0c4, r0c5, r0c6, r0c7, XXX,  r0c9, r0c10, r0c11, r0c12, r0c13, r0c14, r0c15}, \
     {XXX, XXX,  r1c2, r1c3, r1c4, r1c5, r1c6, r1c7, r1c8, r1c9, r1c10, r1c11, r1c12, r1c13, r1c14, r1c15}, \
     {XXX, XXX,  r2c2, r2c3, r2c4, r2c5, r2c6, r2c7, XXX,  r2c9, r2c10, r2c11, r2c12, r2c13, r2c14, r2c15}, \
-    {XXX, r3c1, r3c2, r3c3, r3c4, r3c5, r3c6, r3c7, XXX,  XXX,  r3c10, r3c11, r3c12, r3c13, r3c14, r3c15}, \
+    {XXX, XXX,  r3c2, r3c3, r3c4, r3c5, r3c6, r3c7, XXX,  XXX,  r3c10, r3c11, r3c12, r3c13, r3c14, r3c15}, \
     {XXX, r4c1, r4c2, r4c3, r4c4, r4c5, r4c6, r4c7, r4c8, r4c9, r4c10, r4c11, r4c12, r4c13, r4c14, r4c15}, \
   }
 
